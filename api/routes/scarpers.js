b8bc049d56388827f7391cf94c9f7027b62cbf76
amanster82 <amanbhayani@hotmail.com>
2020-08-07 08:49:10 -0700

    Fixed puppeteer to work on heroku

--- api/routes/scarpers.js
+++ api/routes/scarpers.js
@@ -2,7 +2,12 @@ const puppeteer = require('puppeteer');
 const { raw } = require('express');
 
 async function scrapeCovid(url){
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        ], 
+    });
     const page = await browser.newPage();
     await page.goto(url);
     
